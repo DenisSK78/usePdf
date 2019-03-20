f039d6306c1bffc3c6e0d98b5add125a40e36c7e
Denka <k-d-s@mail.ru>
2019-03-20 12:00:06 +0300

    log ref up up

--- src/main/java/work/usepdf/service/anotation/LoggingUpdateAnnotationPostBeenProcessor.java
+++ src/main/java/work/usepdf/service/anotation/LoggingUpdateAnnotationPostBeenProcessor.java
@@ -3,25 +3,24 @@ package work.usepdf.service.anotation;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.ApplicationContext;
 import work.usepdf.service.LogUpdateService;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LoggingUpdateAnnotationPostBeenProcessor implements BeanPostProcessor {
 
     @Autowired
     private LogUpdateService logUpdateService;
-
-    @Autowired
-    private ApplicationContext context;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -53,18 +52,20 @@ public class LoggingUpdateAnnotationPostBeenProcessor implements BeanPostProcess
                         for (String str : methodsName){
                             if (str.equals(method.getName())){
                                 Class<?>[] parameters = method.getParameterTypes();
-                                for (Class<?> cl: parameters){
-//                                    for (Field field : cl.getDeclaredFields()){
-                                        Method m = cl.getMethod("getId");
-                                        Object o = entityManager.find(cl, m.invoke(args[0]));
-                                        System.out.println(o);
-                                        System.out.println(args[0]);
-//                                    }
-                                }
+                                Class<?> cl = parameters[0];
+                                Method[] methods = cl.getMethods();
+                                List<Method> methodList = Arrays.stream(methods)
+                                        .filter(e -> e.getName().contains("get"))
+                                        .collect(Collectors.toList());
+                                Method m = cl.getMethod("getId");
+                                Object nObj = args[0];
+                                Object oObj = entityManager.find(cl, m.invoke(nObj));
+                                Map<String, Object> diffMap = getDifference(cl, oObj, nObj, methodList);
+                                //toDo: this you can write in db or log!
+                                System.out.println(diffMap);
                             }
                         }
-                        Object original = method.invoke(bean, args);
-                        return original;
+                        return method.invoke(bean, args);
                     });
                 }
             }
@@ -72,5 +73,19 @@ public class LoggingUpdateAnnotationPostBeenProcessor implements BeanPostProcess
         return bean;
     }
 
-
+    /**
+     * @return Map with key: nameField value: value this field
+     * with has changes
+     */
+    private Map<String, Object> getDifference(Object pCl, Object oObj, Object nObj, List<Method> methodList) throws InvocationTargetException, IllegalAccessException {
+        Map<String, Object> diffMap = new HashMap<>();
+        for (Method method : methodList){
+          Object oVal = method.invoke(oObj);
+          Object nVal = method.invoke(nObj);
+          if (!oVal.equals(nVal)){
+              diffMap.put(method.getName(), nVal);
+          }
+        }
+        return diffMap;
+    }
 }
